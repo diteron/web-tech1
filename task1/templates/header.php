5451b6681c498af0b6d3ed4596f6fe0b3ed4e151
Dzmitry Haron <pitbull9846@gmail.com>
2024-05-07 17:34:28 +0300

    Prevent access to templates

--- task1/templates/header.php
+++ task1/templates/header.php
@@ -1,3 +1,10 @@
+<?php
+if ($_SERVER['REQUEST_METHOD'] == 'GET' && realpath(__FILE__) == realpath($_SERVER['SCRIPT_FILENAME'])) {
+    header('HTTP/1.0 403 Forbidden', TRUE, 403);
+    die(header('location: /error.php'));
+}
+?>
+
 <!DOCTYPE html>
 <html lang="ru">
 
@@ -11,21 +18,21 @@
 </head>
 
 <body>
-<div class="header-container">
-    <header>
-        <h1 class="logo">Авто<span style="color: #d8b74c;">Мастер</span></h1>
-        <input type="checkbox" id="nav-toggle" class="nav-toggle">
-        <label for="nav-toggle" class="nav-toggle-hamburger">
-            <i class="fa-solid fa-bars"></i>
-        </label>
-        <nav>
-            <ul>
-                <li><a href="home.php">Главная</a></li>
-                <li><a href="services.php">Услуги</a></li>
-                <li><a href="about.php">О нас</a></li>
-                <li><a href="contacts.php">Контакты</a></li>
-                <li><a href="faq.php">FAQ</a></li>
-            </ul>
-        </nav>
-    </header>
-</div>
+    <div class="header-container">
+        <header>
+            <h1 class="logo">Авто<span style="color: #d8b74c;">Мастер</span></h1>
+            <input type="checkbox" id="nav-toggle" class="nav-toggle">
+            <label for="nav-toggle" class="nav-toggle-hamburger">
+                <i class="fa-solid fa-bars"></i>
+            </label>
+            <nav>
+                <ul>
+                    <li><a href="home.php">Главная</a></li>
+                    <li><a href="services.php">Услуги</a></li>
+                    <li><a href="about.php">О нас</a></li>
+                    <li><a href="contacts.php">Контакты</a></li>
+                    <li><a href="faq.php">FAQ</a></li>
+                </ul>
+            </nav>
+        </header>
+    </div>
