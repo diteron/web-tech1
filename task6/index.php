52bf43c0f8ffe6b0195039ca8ed55738b599cb2b
Dzmitry Haron <pitbull9846@gmail.com>
2024-05-13 02:10:36 +0300

    Complete task6

---
+++ task6/index.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+
+<html lang="ru">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="styles.css">
+    <title>task6</title>
+</head>
+
+<body>
+    <?php
+    date_default_timezone_set("Europe/Minsk");
+
+    $visits = [date("Y-m-d H:i:s")];
+
+    if (isset($_COOKIE["visits"])) {
+        $visits = json_decode($_COOKIE['visits']);
+        $visits[] = date("Y-m-d H:i:s");
+    }
+
+    setcookie("visits", json_encode($visits), strtotime("+7 days"));
+    ?>
+
+    <div class="visits-count">
+        <h2>Your number of site visits: <?php echo count($visits); ?></h2>
+    </div>
+    <div class="visits-time">
+        <h2>List of visits:</h2>
+        <ol>
+            <?php
+            foreach ($visits as $visit) {
+                echo "<li>" . $visit . "</li>";
+            }
+            ?>
+        </ol>
+
+    </div>
+</body>
+
+</html>
